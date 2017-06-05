4b10155263643fecffecd56dab4bc13e216d8f41
Pnevsk123 <vlad.007.p@gmail.com>
2017-06-05 14:05:01 +0300

    first commit

---
+++ Script/DestroyOnContact.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyOnContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+}
